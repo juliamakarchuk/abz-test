205e7a45cf364eab2ee00a70c5c3b6e6eaaa76a0
juliamakarchuk <juliamakarchuk366@gmail.com>
2019-01-09 21:28:32 +0200

    Updates

---
+++ precache-manifest.e90ee5bdd822a069eead70296c1cc7c5.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "799e5af7debe0ab48f7f0a9370f8deb6",
+    "url": "/abz-test/build/static/media/facebook.799e5af7.svg"
+  },
+  {
+    "revision": "6ca4eac2d1041a95f8db",
+    "url": "/abz-test/build/static/css/main.d785fe4a.chunk.css"
+  },
+  {
+    "revision": "eb57c463e70813c363eb",
+    "url": "/abz-test/build/static/js/1.eb57c463.chunk.js"
+  },
+  {
+    "revision": "6f6975a2c329d8e16129",
+    "url": "/abz-test/build/static/js/runtime~main.6f6975a2.js"
+  },
+  {
+    "revision": "16f3c1c2d57888ee3174d252faca43b5",
+    "url": "/abz-test/build/static/media/Layer_8.16f3c1c2.png"
+  },
+  {
+    "revision": "8d603200ff0415e0c6531b5c1ffe4d88",
+    "url": "/abz-test/build/static/media/Layer_9.8d603200.png"
+  },
+  {
+    "revision": "10919850f9b0d50925a13d7db5d3eb21",
+    "url": "/abz-test/build/static/media/logo.10919850.svg"
+  },
+  {
+    "revision": "28b5365a3b3ca4515f2960d6e9a71168",
+    "url": "/abz-test/build/static/media/Layer_8@2x.28b5365a.png"
+  },
+  {
+    "revision": "d882d855ad0c93a45176c40e0dacf8ce",
+    "url": "/abz-test/build/static/media/html.d882d855.svg"
+  },
+  {
+    "revision": "2963787969ffcb9cc7324947abf540d5",
+    "url": "/abz-test/build/static/media/css.29637879.svg"
+  },
+  {
+    "revision": "cf53a810c6682334aa9a2e46a4b86ce4",
+    "url": "/abz-test/build/static/media/javascript.cf53a810.svg"
+  },
+  {
+    "revision": "9ca1b5fb3e4d7d5441b270c02d2a02d9",
+    "url": "/abz-test/build/static/media/Layer_9@2x.9ca1b5fb.png"
+  },
+  {
+    "revision": "557ee6c00b06c2e797044022bc6fd40e",
+    "url": "/abz-test/build/static/media/close-envelope.557ee6c0.svg"
+  },
+  {
+    "revision": "7c043c5ed89e87086b88132c5fec8a27",
+    "url": "/abz-test/build/static/media/telephone-handle-silhouette.7c043c5e.svg"
+  },
+  {
+    "revision": "34e7bb769d2405fc630761ee05f9eb11",
+    "url": "/abz-test/build/static/media/smartphone-call.34e7bb76.svg"
+  },
+  {
+    "revision": "6ca4eac2d1041a95f8db",
+    "url": "/abz-test/build/static/js/main.6ca4eac2.chunk.js"
+  },
+  {
+    "revision": "a7b3c1131e463fae16e601d17e29aca2",
+    "url": "/abz-test/build/static/media/linkedin.a7b3c113.svg"
+  },
+  {
+    "revision": "9411b65c90a9eddeac918a69da282653",
+    "url": "/abz-test/build/static/media/instagram.9411b65c.svg"
+  },
+  {
+    "revision": "3aec60ec1873c36a320df7922c692423",
+    "url": "/abz-test/build/static/media/twitter.3aec60ec.svg"
+  },
+  {
+    "revision": "04a6dc5335c420aa33902c8859b13aa1",
+    "url": "/abz-test/build/static/media/pinterest.04a6dc53.svg"
+  },
+  {
+    "revision": "580da199fd6dd885366d9f1635483ef7",
+    "url": "/abz-test/build/static/media/sign-out-option.580da199.svg"
+  },
+  {
+    "revision": "59b91b321a6042cb94d06b6132f4734a",
+    "url": "/abz-test/build/static/media/Mobile.59b91b32.png"
+  },
+  {
+    "revision": "fb4064189c5b847dec4ad65e2ad24922",
+    "url": "/abz-test/build/static/media/Mobile@2x.fb406418.png"
+  },
+  {
+    "revision": "a1271bda2f4924c6538b146f55b55a03",
+    "url": "/abz-test/build/static/media/Tablet.a1271bda.png"
+  },
+  {
+    "revision": "4af1ed4e74141c154d77f4fa8b0f3711",
+    "url": "/abz-test/build/static/media/Tablet@2x.4af1ed4e.png"
+  },
+  {
+    "revision": "3da6d2f807e3b9075bc0c0e8cb4f1f2c",
+    "url": "/abz-test/build/static/media/Desktop.3da6d2f8.png"
+  },
+  {
+    "revision": "3da6d2f807e3b9075bc0c0e8cb4f1f2c",
+    "url": "/abz-test/build/static/media/Desktop@2x.3da6d2f8.png"
+  },
+  {
+    "revision": "a4c720dd4a83a2811540ecfc49df3034",
+    "url": "/abz-test/build/static/media/2K.a4c720dd.png"
+  },
+  {
+    "revision": "0f8381471d20f5f2f2d35992d8df94bf",
+    "url": "/abz-test/build/static/media/2K@2x.0f838147.png"
+  },
+  {
+    "revision": "e66db5fd1a83c30f66a588a6d74f63da",
+    "url": "/abz-test/build/static/media/bg-2.e66db5fd.jpg"
+  },
+  {
+    "revision": "9d202bb065cc2ff05f7727f3b36c32f8",
+    "url": "/abz-test/build/index.html"
+  }
+];
